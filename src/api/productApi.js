86e93758c36b6030c1daf1e8f13e275bbf165b18
aass2346789 <aass2346789@gmail.com>
2025-07-08 19:45:00 +0800

    調整api網址

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/product',
+  baseURL: import.meta.env.VITE_API_CART_BASE_URL + 'product',
 });
 
 export default {
